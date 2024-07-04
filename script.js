d0bd17b4a6383e1d43af08e5ef3aa54498587d4d
kaustubha-saxena <75664233+kaustubha-saxena@users.noreply.github.com>
2024-07-05 01:33:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+let limit = document.body.querySelector(".limit");
+let Length = document.body.querySelector(".length");
+let Lower_case = document.querySelector("#Lower-Case");
+let Upper_case = document.querySelector("#Upper-Case");
+let Numbers = document.querySelector("#Numbers");
+let Symbols = document.querySelector("#Symbols");
+var Text = document.querySelector(".text");
+let btn = document.body.querySelector(".btn");
+let password = "";
+var value;
+
+limit.children[1].value = 14;
+value = Math.round(14 / 100 * 28) + 4;
+Lower_case.checked=true;
+Upper_case.checked=true;
+Numbers.checked=true;
+Symbols.checked=true;
+
+limit.children[1].addEventListener("change", () => {
+    value = limit.children[1].value;
+    value = Math.round(value / 100 * 28) + 4;
+    Length.innerHTML = `Length: ${value} `;
+})
+function generate_char(a) {
+    return String.fromCharCode(a);
+}
+function generate_password() {
+password="";
+
+    for (i = 1; i <= value; i++) {
+        let integer = Math.floor(Math.random() * 89 + 33);
+        if (Lower_case.checked == true && integer >= 97 && integer <= 122) {
+            let char = generate_char(integer);
+            password = password.concat(char);
+        }
+        else if (Upper_case.checked == true && integer >= 65 && integer <= 90) {
+            let char = generate_char(integer);
+            password = password.concat(char);
+        }
+        else if (Numbers.checked == true && integer >= 48 && integer <= 57) {
+            let char = generate_char(integer);
+            password = password.concat(char);
+        }
+        else if (Symbols.checked == true && integer >= 33 && integer <= 47 || Symbols.checked == true && integer >= 58 && integer <= 64) {
+            let char = generate_char(integer);
+            password = password.concat(char);
+        }
+        else {
+            i--;
+        }
+    }
+    Text.innerHTML="";
+
+    Text.innerHTML = password;
+    
+}
+
+    const copy = async () => {
+        try {
+          await navigator.clipboard.writeText(password);
+          console.log('Content copied to clipboard');
+        } catch (err) {
+          console.error('Failed to copy: ', err);
+        }
+      }
+    
+
+
+
+
